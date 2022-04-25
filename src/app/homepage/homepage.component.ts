91c6c3ec9e51ec68cbf87223f17393070b892fbc
Nikolina Pelivan <nikolina_pelivan@hotmail.com>
2022-04-25 17:13:30 +0200

    show image on click compleated

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,6 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
   popupImage = false;
 
+  image: any = '../../assets/images/image-product-1.jpg';
+
+  firstImage() {
+    this.image = '../../assets/images/image-product-1.jpg';
+  }
+
+  secondImage() {
+    this.image = '../../assets/images/image-product-2.jpg';
+  }
+
+  thirdImage() {
+    this.image = '../../assets/images/image-product-3.jpg';
+  }
+
+  fourthImage() {
+    this.image = '../../assets/images/image-product-4.jpg';
+  }
+
+  nextImage() {
+    if (this.image === '../../assets/images/image-product-1.jpg') {
+      this.secondImage();
+      debugger;
+    }
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
